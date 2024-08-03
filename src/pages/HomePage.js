7522133e1cc64048581f2746918a6f23a904565a
Peter Huynh <peterhyh@yahoo.com>
2024-08-02 21:03:33 -0500

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import mountain from '../assets/img/mountain_cliff.png';
+import pic from '../assets/img/me.jpg';
+
+const HomePage = () => {
+    return (
+        <div className='template'>
+            <div className='card'>
+                <img className='card-img' src={mountain} alt='mountain' />
+            </div>
+            <div className='card'>
+                <div className='card-inner-border'>
+                    <div className='profile-pic'>
+                        <img src={pic} alt='picture' />
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default HomePage;
